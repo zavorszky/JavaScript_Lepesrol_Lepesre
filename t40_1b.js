5f1c7193761ffd67b9790faf2e08529dc4cc4e47
zavorszky <zavorszky@yahoo.com>
2018-07-01 23:48:32 +0200

    Help gomb OK

--- t40_1b.js
+++ t40_1b.js
@@ -20,7 +20,23 @@ function t40_1b_ini() {
     window.alert("Nincs csatlakozva a kiszolgálóhoz!");
     return fales;
   };
-} // t30_1b_ini
-
+  var v_button_elem = document.getElementById("button_segites");
+  v_button_elem.onclick = function() {
+    if (typeof v_ablak_segites == "undefined" || v_ablak_segites.closed) {
+      v_ablak_segites = window.open("", "", "menubar=no, toolbar=no, status=no, width=310, height=300");
+      v_ablak_segites.document.write("<p>A bejelentkezéshez kötelező megadni a regisztácónál használt</p>");
+      v_ablak_segites.document.write("<ul>");
+      v_ablak_segites.document.write("<li>Felhasználó azonosítót (Felhasználó),</li>");
+      v_ablak_segites.document.write("<li>Jelszót.</li>");
+      v_ablak_segites.document.write("</ul>");
+      v_ablak_segites.document.write("<p>A VIP megadása nem kötelező, de ha van VIP kódja,");
+      v_ablak_segites.document.write(" akkor a szokásosnál több lehetőség áll rendelkezésére.</p>");
+      v_ablak_segites.document.title = "Kitöltési útmutató";
+    } else {
+      window.alert("Figyelem!\nA kitöltési útmutató már nyitva.");
+    }
+    return false;
+  };
+} // t40_1b_ini
 //window.onload = t30_1b_ini;
 window.addEventListener("load", t40_1b_ini);
